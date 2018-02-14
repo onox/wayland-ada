80f131b0b75e7bf49e4670ce3293c304853cbd34
Joakim Strandberg <joakim@mequinox.se>
2018-02-14 23:11:16 +0100

    Made the examples 6.3 and 6.4 resemble the C versions

--- src/example_6_3_connect_to_server.adb
+++ src/example_6_3_connect_to_server.adb
@@ -4,32 +4,15 @@ with Ada.Text_IO;
 -- See section 6.3 at:
 -- https://jan.newmarch.name/Wayland/ProgrammingClient/
 procedure Example_6_3_Connect_To_Server is
-
    Display : Wl.Display_T;
-
-   procedure Do_Something;
-
-   procedure Connect_To_Wayland_Server is
-   begin
-      Display.Connect (Wl.Default_Display_Name);
-      if Display.Is_Connected then
-         Ada.Text_IO.Put_Line ("Success");
-         Do_Something;
-         Display.Disconnect;
-      else
-         Ada.Text_IO.Put_Line ("Failure");
-      end if;
-
-      pragma Assert (not Display.Is_Connected);
-   end Connect_To_Wayland_Server;
-
-   pragma Unmodified (Display);
-
-   procedure Do_Something is
-   begin
-      null;
-   end Do_Something;
-
 begin
-   Connect_To_Wayland_Server;
+   Display.Connect (Wl.Default_Display_Name);
+   if not Display.Is_Connected then
+      Ada.Text_IO.Put_Line ("Can't connect to display");
+      return;
+   end if;
+   Ada.Text_IO.Put_Line ("Connected to display");
+
+   Display.Disconnect;
+   Ada.Text_IO.Put_Line ("Disconnected from display");
 end Example_6_3_Connect_To_Server;
--- src/example_6_4_find_compositor_proxy.adb
+++ src/example_6_4_find_compositor_proxy.adb
@@ -8,75 +8,40 @@ procedure Example_6_4_Find_Compositor_Proxy is
    package Subscriber is new Wl.Global_Objects_Subscriber;
 
    Compositor : Wl.Compositor_T;
-
-   Display : Wl.Display_T;
-
-   procedure Get_Registry;
-
-   procedure Connect_To_Wayland_Server is
-   begin
-      Display.Connect (Wl.Default_Display_Name);
-      if Display.Is_Connected then
-         Ada.Text_IO.Put_Line ("Successfully connected to wayland server");
-         Get_Registry;
-
---         Wl.Clear_Global_Objects;
-
-         Display.Disconnect;
-         Ada.Text_IO.Put_Line ("Disconnected from wayland server");
-      else
-         Ada.Text_IO.Put_Line ("Failed to connect to wayland server");
-      end if;
-
-      pragma Assert (not Display.Is_Connected);
-   end Connect_To_Wayland_Server;
-
-   pragma Unmodified (Display);
-
-   Registry : Wl.Registry_T;
-
-   procedure Get_Global_Objects_Available_In_Registry;
-
-   procedure Get_Registry is
-   begin
-      Registry.Get (Display);
-      if Registry.Has_Registry_Object then
-         Get_Global_Objects_Available_In_Registry;
-         Registry.Destroy;
-      else
-         Ada.Text_IO.Put_Line ("Failed to retrieve Registry!!!!");
-      end if;
-
-      pragma Assert (not Registry.Has_Registry_Object);
-   end Get_Registry;
-
-   procedure Check_If_Compositor_Found;
-
-   procedure Get_Global_Objects_Available_In_Registry is
-   begin
-      Subscriber.Start_Subscription (Registry, Display);
-
-      Check_If_Compositor_Found;
-   end Get_Global_Objects_Available_In_Registry;
-
-   pragma Unmodified (Registry);
-
-   procedure Check_If_Compositor_Found is
-   begin
-      for Global_Object of Subscriber.Global_Objects loop
-         if Global_Object.Interface_Name = "wl_compositor" then
-            Compositor.Bind (Registry, Global_Object.Id, 1);
-            exit;
-         end if;
-      end loop;
-
-      if Compositor.Is_Bound then
-         Ada.Text_IO.Put_Line ("Found compositor");
-      else
-         Ada.Text_IO.Put_Line ("Can't find compositor");
-      end if;
-   end Check_If_Compositor_Found;
+   Display    : Wl.Display_T;
+   Registry   : Wl.Registry_T;
 
 begin
-   Connect_To_Wayland_Server;
+   Display.Connect (Wl.Default_Display_Name);
+   if not Display.Is_Connected then
+      Ada.Text_IO.Put_Line ("Can't connect to display");
+      return;
+   end if;
+   Ada.Text_IO.Put_Line ("Connected to display");
+
+   Registry.Get (Display);
+   if not Registry.Has_Registry_Object then
+      Ada.Text_IO.Put_Line ("Can't get global registry object");
+      return;
+   end if;
+
+   Subscriber.Start_Subscription (Registry, Display);
+
+   for Global_Object of Subscriber.Global_Objects loop
+      Ada.Text_IO.Put ("Got a registry event for ");
+      Ada.Text_IO.Put_Line (Global_Object.Interface_Name & " id" & Global_Object.Id'Image);
+      if Global_Object.Interface_Name = "wl_compositor" then
+         Compositor.Bind (Registry, Global_Object.Id, 1);
+      end if;
+   end loop;
+
+   if Compositor.Is_Bound then
+      Ada.Text_IO.Put_Line ("Found compositor");
+   else
+      Ada.Text_IO.Put_Line ("Can't find compositor");
+   end if;
+
+   Registry.Destroy;
+   Display.Disconnect;
+   Ada.Text_IO.Put_Line ("Disconnected from display");
 end Example_6_4_Find_Compositor_Proxy;
