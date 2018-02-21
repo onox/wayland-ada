e1ce7cd60b01e4fd61831dbd4f87ca90424b4315
Joakim Strandberg <joakim@mequinox.se>
2018-02-21 19:49:02 +0100

    Added memory mapping functionality in the thick Ada binding to the Ubuntu posix

--- src/px.adb
+++ src/px.adb
@@ -4,16 +4,15 @@ package body Px is
 
    use type Interfaces.C.int;
 
-   procedure Open (File      : in out File_T;
-                   File_Name : in     C_String;
-                   Flags     : in     O_FLag_T;
-                   S_Flags   : in     S_FLag_T)
+   procedure Open
+     (File      : in out File_T;
+      File_Name : in     C_String;
+      Flags     : in     O_FLag_T;
+      S_Flags   : in     S_FLag_T)
    is
    begin
-      File.My_File_Descriptor := Px_Thin.Open (File_Name,
-                                               Flags,
-                                               S_Flags);
-      File.My_Is_Open := File.My_File_Descriptor /= 0;
+      File.My_File_Descriptor := Px_Thin.Open (File_Name, Flags, S_Flags);
+      File.My_Is_Open         := File.My_File_Descriptor /= 0;
    end Open;
 
    procedure Close (File : in out File_T) is
@@ -22,30 +21,59 @@ package body Px is
       File.My_Is_Open := False;
    end Close;
 
-   procedure Get_File_Status (File        : in     File_T;
-                              File_Status : in out Status_T)
+   procedure Get_File_Status
+     (File        : in     File_T;
+      File_Status : in out Status_T)
    is
-      Result : constant int := Px_Thin.Get_File_Status
-        (Fd     => File.My_File_Descriptor,
-         Status => File_Status.My_Status'Access);
+      Result : constant int :=
+        Px_Thin.Get_File_Status
+          (Fd     => File.My_File_Descriptor,
+           Status => File_Status.My_Status'Access);
    begin
       File_Status.My_Is_Valid := Result = 0;
    end Get_File_Status;
 
-   procedure Write (File  : File_T;
-                    Bytes : Byte_Array_T)
-   is
+   procedure Write (File : File_T; Bytes : Byte_Array_T) is
       SSize : SSize_T;
    begin
-      SSize := Px_Thin.Write (File_Descriptor => File.My_File_Descriptor,
-                              Buffer          => Bytes,
-                              Count           => Bytes'Length);
+      SSize :=
+        Px_Thin.Write
+          (File_Descriptor => File.My_File_Descriptor,
+           Buffer          => Bytes,
+           Count           => Bytes'Length);
    end Write;
 
-   function Read (File  : File_T;
-                  Bytes : Byte_Array_T) return SSize_T is
+   function Read (File : File_T; Bytes : Byte_Array_T) return SSize_T is
    begin
       return Px_Thin.Read (File.My_File_Descriptor, Bytes, Bytes'Length);
    end Read;
+
+   procedure Memory_Map
+     (File    : in File_T;
+      Address : Void_Ptr;
+      Len     : Size_T;
+      Prot    : Prot_FLag_T;
+      Flags   : int;
+      Offset  : Offset_T;
+      Memory_Map : in out Memory_Map_T) is
+   begin
+      Memory_Map.My_Mapping := Px_Thin.Mmap (Address,
+                                             Len,
+                                             Prot,
+                                             Flags,
+                                             File.My_File_Descriptor,
+                                             Offset);
+      Memory_Map.My_Length := Len;
+   end Memory_Map;
+
+   function Memory_Unmap (Map : in out Memory_Map_T) return int is
+      R : int;
+   begin
+      R := Px_Thin.Munmap (Map.My_Mapping, Map.My_Length);
+      if R = 0 then
+         Map.My_Mapping := MAP_FAILED;
+      end if;
+      return R;
+   end Memory_Unmap;
 
 end Px;
--- src/px.ads
+++ src/px.ads
@@ -1,10 +1,13 @@
 with Interfaces.C.Strings;
 with System.Storage_Elements;
 
+private with Ada.Unchecked_Conversion;
+
 package Px is
 
    type File_T;
    type Status_T;
+   type Memory_Map_T;
 
    use type Interfaces.Unsigned_32;
 
@@ -15,20 +18,28 @@ package Px is
    subtype Unsigned_32 is Interfaces.Unsigned_32;
    subtype chars_ptr is Interfaces.C.Strings.chars_ptr;
 
+   subtype Void_Ptr is System.Address;
+
    type S_FLag_T is new Unsigned_32;
    type O_FLag_T is new Unsigned_32;
+   type Prot_FLag_T is new Unsigned_32;
 
    function Shift_Right
      (Value  : Unsigned_32;
-      Amount : Natural) return Unsigned_32 renames Interfaces.Shift_Right;
+      Amount : Natural) return Unsigned_32 renames
+     Interfaces.Shift_Right;
 
-   function Shift_Right (Value  : S_FLag_T;
-                         Amount : Natural) return S_FLag_T
-   is
+   function Shift_Right
+     (Value  : S_FLag_T;
+      Amount : Natural) return S_FLag_T is
      (S_FLag_T (Shift_Right (Unsigned_32 (Value), Amount)));
 
    use type S_FLag_T;
    use type O_FLag_T;
+
+   use type int;
+   use type long;
+   use type Void_Ptr;
 
    --
    -- Encoding of the file mode.
@@ -40,12 +51,12 @@ package Px is
    -- File types
    --
 
-   S_IFDIR : constant S_FLag_T := 0040000; -- Directory.
-   S_IFCHR : constant S_FLag_T := 0020000; -- Character device.
-   S_IFBLK : constant S_FLag_T := 0060000; -- Block device.
-   S_IFREG : constant S_FLag_T := 0100000; -- Regular file.
-   S_IFIFO : constant S_FLag_T := 0010000; -- FIFO.
-   S_IFLNK : constant S_FLag_T := 0120000; -- Symbolic link.
+   S_IFDIR  : constant S_FLag_T := 0040000; -- Directory.
+   S_IFCHR  : constant S_FLag_T := 0020000; -- Character device.
+   S_IFBLK  : constant S_FLag_T := 0060000; -- Block device.
+   S_IFREG  : constant S_FLag_T := 0100000; -- Regular file.
+   S_IFIFO  : constant S_FLag_T := 0010000; -- FIFO.
+   S_IFLNK  : constant S_FLag_T := 0120000; -- Symbolic link.
    S_IFSOCK : constant S_FLag_T := 0140000; -- Socket.
 
    --  #define  __S_ISUID   04000  /* Set user ID on execution.  */
@@ -92,13 +103,13 @@ package Px is
    S_IRWXO : constant S_FLag_T := Shift_Right (S_IRWXG, 3);
 
    -- Open for reading only
-   O_RDONLY  : constant O_FLag_T := 16#00#;
+   O_RDONLY : constant O_FLag_T := 16#00#;
 
    -- Open for write only
-   O_WRONLY  : constant O_FLag_T := 16#01#;
+   O_WRONLY : constant O_FLag_T := 16#01#;
 
    -- Open for reading and writing
-   O_RDWR    : constant O_FLag_T := 16#02#;
+   O_RDWR : constant O_FLag_T := 16#02#;
 
    O_ACCMODE : constant O_FLag_T := 16#03#;
 
@@ -118,33 +129,63 @@ package Px is
 
    O_ASYNC : constant O_FLag_T := 020000;
 
+   -- Protections are chosen from these bits, OR'd together.  The
+   -- implementation does not necessarily support PROT_EXEC or PROT_WRITE
+   -- without PROT_READ.  The only guarantees are that no writing will be
+   -- allowed without PROT_WRITE and no access will be allowed for PROT_NONE.
+
    -- Page can be read.
-   PROT_READ : constant := 16#1#;
+   PROT_READ : constant Prot_FLag_T := 16#1#;
 
    -- Page can be written.
-   PROT_WRITE : constant := 16#2#;
+   PROT_WRITE : constant Prot_FLag_T := 16#2#;
 
    -- Page can be executed.
-   PROT_EXEC : constant := 16#4#;
+   PROT_EXEC : constant Prot_FLag_T := 16#4#;
 
    -- Page can not be accessed.
-   PROT_NONE : constant := 16#0#;
+   PROT_NONE : constant Prot_FLag_T := 16#0#;
 
    -- Extend change to start of growsdown vma (mprotect only).
-   PROT_GROWSDOWN : constant := 16#01000000#;
+   PROT_GROWSDOWN : constant Prot_FLag_T := 16#01000000#;
 
    -- Extend change to start of growsup vma (mprotect only).
-   PROT_GROWSUP : constant := 16#02000000#;
+   PROT_GROWSUP : constant Prot_FLag_T := 16#02000000#;
 
    --
    -- Sharing types (must choose one and only one of these).
    --
 
    -- Share changes.
-   MAP_SHARED  : constant := 16#01#;
+   MAP_SHARED : constant := 16#01#;
 
    -- Changes are private.
    MAP_PRIVATE : constant := 16#02#;
+
+   -- Interpret addr exactly.
+   MAP_FIXED : constant :=  16#10#;
+
+   -- Don't use a file.
+   MAP_ANON : constant := 16#20#;
+
+   MAP_HUGE_SHIFT : constant := 26;
+
+   MAP_HUGE_MASK : constant := 16#3f#;
+
+   MAP_FAILED : constant Void_Ptr;
+
+   --
+   -- Flags to `msync'.
+   --
+
+   -- Sync memory asynchronously.
+   MS_ASYNC : constant := 1;
+
+   -- Synchronous memory sync.
+   MS_SYNC : constant := 4;
+
+   -- Invalidate the caches.
+   MS_INVALIDATE : constant := 2;
 
    subtype Device_Id_T is unsigned_long;
 
@@ -170,14 +211,15 @@ package Px is
 
    subtype Time_Nano_Sec_T is long;
 
-   subtype Void_Ptr is System.Address;
+   Nil : Void_Ptr renames System.Null_Address;
 
-   subtype Off_T is long;
+   subtype Offset_T is long;
 
    Nul : constant Character := Character'Val (0);
 
-   subtype C_String is String with Dynamic_Predicate =>
-     C_String'Length > 0 and then C_String (C_String'Last) = Nul;
+   subtype C_String is String with
+        Dynamic_Predicate => C_String'Length > 0
+        and then C_String (C_String'Last) = Nul;
 
    subtype Byte_T is System.Storage_Elements.Storage_Element;
 
@@ -187,101 +229,126 @@ package Px is
       Sec      : aliased Time_Sec_T;
       Nano_Sec : aliased Time_Nano_Sec_T;
    end record with
-     Convention => C_Pass_By_Copy;
+      Convention => C_Pass_By_Copy;
 
    type File_T is tagged limited private;
 
-   procedure Open (File      : in out File_T;
-                   File_Name : in     C_String;
-                   Flags     : in     O_FLag_T;
-                   S_Flags   : in     S_FLag_T) with
-     Global => null,
-     Pre    => File.Is_Closed;
+   procedure Open
+     (File      : in out File_T;
+      File_Name : in     C_String;
+      Flags     : in     O_FLag_T;
+      S_Flags   : in     S_FLag_T) with
+      Global => null,
+      Pre    => File.Is_Closed;
 
    procedure Close (File : in out File_T) with
-     Global => null,
-     Pre    => File.Is_Open,
-     Post   => File.Is_Closed;
+      Global => null,
+      Pre    => File.Is_Open,
+      Post   => File.Is_Closed;
 
-   procedure Write (File  : File_T;
-                    Bytes : Byte_Array_T) with
-     Global => null,
-     Pre    => File.Is_Open;
+   procedure Write (File : File_T; Bytes : Byte_Array_T) with
+      Global => null,
+      Pre    => File.Is_Open;
 
-   function Read (File  : File_T;
-                  Bytes : Byte_Array_T) return SSize_T with
-     Global => null,
-     Pre    => File.Is_Open;
+   function Read (File : File_T; Bytes : Byte_Array_T) return SSize_T with
+      Global => null,
+      Pre    => File.Is_Open;
 
-   procedure Get_File_Status (File        : in     File_T;
-                              File_Status : in out Status_T) with
+   procedure Get_File_Status
+     (File        : in     File_T;
+      File_Status : in out Status_T) with
+      Global => null,
+      Pre    => File.Is_Open;
+
+   procedure Memory_Map
+     (File    : in File_T;
+      Address : Void_Ptr;
+      Len     : Size_T;
+      Prot    : Prot_FLag_T;
+      Flags   : int;
+      Offset  : Offset_T;
+      Memory_Map : in out Memory_Map_T) with
      Global => null,
-     Pre    => File.Is_Open;
+     Pre    => not Has_Mapping (Memory_Map);
 
    function Is_Open (File : File_T) return Boolean with
-     Global => null;
+      Global => null;
 
    function Is_Closed (File : File_T) return Boolean with
-     Global => null;
+      Global => null;
 
    type Status_T is tagged limited private;
 
    function Is_Valid (Status : Status_T) return Boolean with
-     Global => null;
+      Global => null;
 
    function Device_Id (Status : Status_T) return Device_Id_T with
-     Global => null,
-     Pre    => Status.Is_Valid;
+      Global => null,
+      Pre    => Status.Is_Valid;
 
    function Inode_Number (Status : Status_T) return Inode_Number_T with
-     Global => null,
-     Pre    => Status.Is_Valid;
+      Global => null,
+      Pre    => Status.Is_Valid;
 
    function Hard_Link_Count (Status : Status_T) return Hard_Link_Count_T with
-     Global => null,
-     Pre    => Status.Is_Valid;
+      Global => null,
+      Pre    => Status.Is_Valid;
 
    function Mode (Status : Status_T) return Mode_T with
-     Global => null,
-     Pre    => Status.Is_Valid;
+      Global => null,
+      Pre    => Status.Is_Valid;
 
    function User_Id (Status : Status_T) return User_Id_T with
-     Global => null,
-     Pre    => Status.Is_Valid;
+      Global => null,
+      Pre    => Status.Is_Valid;
 
    function Group_Id (Status : Status_T) return Group_Id_T with
-     Global => null,
-     Pre    => Status.Is_Valid;
+      Global => null,
+      Pre    => Status.Is_Valid;
 
    function Special_Device_Id (Status : Status_T) return Device_Id_T with
-     Global => null,
-     Pre    => Status.Is_Valid;
+      Global => null,
+      Pre    => Status.Is_Valid;
 
-   function Size (Status : Status_T) return Off_T with
-     Global => null,
-     Pre    => Status.Is_Valid;
+   function Size (Status : Status_T) return Offset_T with
+      Global => null,
+      Pre    => Status.Is_Valid;
 
    function Block_Size (Status : Status_T) return Block_Size_T with
-     Global => null,
-     Pre    => Status.Is_Valid;
+      Global => null,
+      Pre    => Status.Is_Valid;
 
-   -- Number of 512B blocks allocated
+      -- Number of 512B blocks allocated
    function Block_Count (Status : Status_T) return Block_Size_T with
-     Global => null,
-     Pre    => Status.Is_Valid;
+      Global => null,
+      Pre    => Status.Is_Valid;
 
    function Last_Access_Time (Status : Status_T) return Time_T with
-     Global => null,
-     Pre    => Status.Is_Valid;
+      Global => null,
+      Pre    => Status.Is_Valid;
 
    function Modification_Time (Status : Status_T) return Time_T with
-     Global => null,
-     Pre    => Status.Is_Valid;
+      Global => null,
+      Pre    => Status.Is_Valid;
 
-   -- Last status change time
+      -- Last status change time
    function Change_Time (Status : Status_T) return Time_T with
+      Global => null,
+      Pre    => Status.Is_Valid;
+
+   type Memory_Map_T is tagged limited private;
+
+   function Has_Mapping (Map : Memory_Map_T) return Boolean with
+     Global => null;
+
+   function Mapping (Map : Memory_Map_T) return Void_Ptr with
      Global => null,
-     Pre    => Status.Is_Valid;
+     Pre    => Map.Has_Mapping;
+
+   -- Returns 0 on success, otherwise -1.
+   function Memory_Unmap (Map : in out Memory_Map_T) return int with
+     Global => null,
+     Post   => (if Memory_Unmap'Result = 0 then not Map.Has_Mapping);
 
 private
 
@@ -289,99 +356,103 @@ private
 
       type File_Status_T is record
          -- ID of device containing file
-         Device_Id         : aliased Device_Id_T;
+         Device_Id : aliased Device_Id_T;
 
-         Inode_Number      : aliased Inode_Number_T;
-         Hard_Link_Count   : aliased Hard_Link_Count_T;
+         Inode_Number    : aliased Inode_Number_T;
+         Hard_Link_Count : aliased Hard_Link_Count_T;
 
          -- Protection
-         Mode              : aliased Mode_T;
+         Mode : aliased Mode_T;
 
-         User_Id           : aliased User_Id_T;
-         Group_Id          : aliased Group_Id_T;
-         Padding_0         : aliased int;
+         User_Id   : aliased User_Id_T;
+         Group_Id  : aliased Group_Id_T;
+         Padding_0 : aliased int;
 
          -- Device ID (if special file)
          Special_Device_Id : aliased Device_Id_T;
 
          -- Total size, in bytes
-         Size              : aliased Off_T;
+         Size : aliased Offset_T;
 
          -- Blocksize for file system I/O
-         Block_Size        : aliased Block_Size_T;
+         Block_Size : aliased Block_Size_T;
 
          -- Number of 512B blocks allocated
-         Block_Count       : aliased Block_Count_T;
+         Block_Count : aliased Block_Count_T;
 
          -- Time of last access
-         Access_Time       : aliased Time_T;
+         Access_Time : aliased Time_T;
 
          -- Time of last modification
          Modification_Time : aliased Time_T;
 
          -- Time of last status change
-         Change_Time       : aliased Time_T;
-         Padding_1         : long;
-         Padding_2         : long;
-         Padding_3         : long;
+         Change_Time : aliased Time_T;
+         Padding_1   : long;
+         Padding_2   : long;
+         Padding_3   : long;
       end record with
-        Convention => C_Pass_By_Copy;
+         Convention => C_Pass_By_Copy;
 
-      function Get_File_Status (Fd     : int;
-                                Status : access File_Status_T) return int with
-        Import        => True,
-        Convention    => C,
-        External_Name => "fstat";
+      function Get_File_Status
+        (Fd     : int;
+         Status : access File_Status_T) return int with
+         Import        => True,
+         Convention    => C,
+         External_Name => "fstat";
 
-      -- Establishes a connection between a file and a file descriptor.
-      -- The file descriptor handle (a non-negative number)
-      -- is returned upon success, otherwise -1.
-      --
+         -- Establishes a connection between a file and a file descriptor.
+         -- The file descriptor handle (a non-negative number)
+         -- is returned upon success, otherwise -1.
+         --
       -- Applications shall specify exactly one of the first three flags:
       -- O_RDONLY, O_WRONLY and O_RDWR. And then any combination of O_APPEND,
       -- O_CREAT, O_DSYNC, O_EXCL, O_NOCTTY, O_NONBLOCK, O_RSYNC,
       -- O_SYNC, O_TRUNC.
-      function Open (File_Name : C_String;
-                     Flags     : O_FLag_T;
-                     S_Flags   : S_FLag_T) return int with
-        Import        => True,
-        Convention    => C,
-        External_Name => "open";
+      function Open
+        (File_Name : C_String;
+         Flags     : O_FLag_T;
+         S_Flags   : S_FLag_T) return int with
+         Import        => True,
+         Convention    => C,
+         External_Name => "open";
 
       procedure Close (File_Descriptor : int) with
-        Import        => True,
-        Convention    => C,
-        External_Name => "close";
+         Import        => True,
+         Convention    => C,
+         External_Name => "close";
 
-      function Write (File_Descriptor : int;
-                      Buffer          : Byte_Array_T;
-                      Count           : Size_T) return SSize_T with
-        Import        => True,
-        Convention    => C,
-        External_Name => "write";
+      function Write
+        (File_Descriptor : int;
+         Buffer          : Byte_Array_T;
+         Count           : Size_T) return SSize_T with
+         Import        => True,
+         Convention    => C,
+         External_Name => "write";
 
-      function Read (File_Descriptor : int;
-                     Buffer          : Byte_Array_T;
-                     Count           : Size_T) return SSize_T with
-        Import        => True,
-        Convention    => C,
-        External_Name => "read";
+      function Read
+        (File_Descriptor : int;
+         Buffer          : Byte_Array_T;
+         Count           : Size_T) return SSize_T with
+         Import        => True,
+         Convention    => C,
+         External_Name => "read";
 
-      function Mmap (Addr   : Void_Ptr;
-                     Len    : Size_T;
-                     Prot   : int;
-                     Flags  : int;
-                     Fd     : int;
-                     Offset : Off_T) return Void_Ptr with
-        Import        => True,
-        Convention    => C,
-        External_Name => "mmap";
+      function Mmap
+        (Addr   : Void_Ptr;
+         Len    : Size_T;
+         Prot   : Prot_FLag_T;
+         Flags  : int;
+         Fd     : int;
+         Offset : Offset_T) return Void_Ptr with
+         Import        => True,
+         Convention    => C,
+         External_Name => "mmap";
 
-      function Munmap (Addr   : Void_Ptr;
-                       Length : Size_T) return int with
-        Import        => True,
-        Convention    => C,
-        External_Name => "munmap";
+      function Munmap (Addr : Void_Ptr; Length : Size_T) return int with
+         Import        => True,
+         Convention    => C,
+         External_Name => "munmap";
 
    end Px_Thin;
 
@@ -399,32 +470,73 @@ private
       My_Is_Valid : Boolean := False;
    end record;
 
-   function Is_Valid (Status : Status_T) return Boolean is (Status.My_Is_Valid);
+   function Is_Valid
+     (Status : Status_T) return Boolean is
+     (Status.My_Is_Valid);
 
-   function Device_Id (Status : Status_T) return Device_Id_T is (Status.My_Status.Device_Id);
+   function Device_Id
+     (Status : Status_T) return Device_Id_T is
+     (Status.My_Status.Device_Id);
 
-   function Inode_Number (Status : Status_T) return Inode_Number_T is (Status.My_Status.Inode_Number);
+   function Inode_Number
+     (Status : Status_T) return Inode_Number_T is
+     (Status.My_Status.Inode_Number);
 
-   function Hard_Link_Count (Status : Status_T) return Hard_Link_Count_T is (Status.My_Status.Hard_Link_Count);
+   function Hard_Link_Count
+     (Status : Status_T) return Hard_Link_Count_T is
+     (Status.My_Status.Hard_Link_Count);
 
    function Mode (Status : Status_T) return Mode_T is (Status.My_Status.Mode);
 
-   function User_Id (Status : Status_T) return User_Id_T is (Status.My_Status.User_Id);
+   function User_Id
+     (Status : Status_T) return User_Id_T is
+     (Status.My_Status.User_Id);
 
-   function Group_Id (Status : Status_T) return Group_Id_T is (Status.My_Status.Group_Id);
+   function Group_Id
+     (Status : Status_T) return Group_Id_T is
+     (Status.My_Status.Group_Id);
 
-   function Special_Device_Id (Status : Status_T) return Device_Id_T is (Status.My_Status.Special_Device_Id);
+   function Special_Device_Id
+     (Status : Status_T) return Device_Id_T is
+     (Status.My_Status.Special_Device_Id);
 
-   function Size (Status : Status_T) return Off_T is (Status.My_Status.Size);
+   function Size
+     (Status : Status_T) return Offset_T is
+     (Status.My_Status.Size);
 
-   function Block_Size (Status : Status_T) return Block_Size_T is (Status.My_Status.Block_Size);
+   function Block_Size
+     (Status : Status_T) return Block_Size_T is
+     (Status.My_Status.Block_Size);
 
-   function Block_Count (Status : Status_T) return Block_Size_T is (Status.My_Status.Block_Count);
+   function Block_Count
+     (Status : Status_T) return Block_Size_T is
+     (Status.My_Status.Block_Count);
 
-   function Last_Access_Time (Status : Status_T) return Time_T is (Status.My_Status.Access_Time);
+   function Last_Access_Time
+     (Status : Status_T) return Time_T is
+     (Status.My_Status.Access_Time);
 
-   function Modification_Time (Status : Status_T) return Time_T is (Status.My_Status.Modification_Time);
+   function Modification_Time
+     (Status : Status_T) return Time_T is
+     (Status.My_Status.Modification_Time);
 
-   function Change_Time (Status : Status_T) return Time_T is (Status.My_Status.Change_Time);
+   function Change_Time
+     (Status : Status_T) return Time_T is
+     (Status.My_Status.Change_Time);
+
+   function Conv is new Ada.Unchecked_Conversion (Source => long,
+                                                  Target => Void_Ptr);
+
+   MAP_FAILED_VALUE : constant long     := -1;
+   MAP_FAILED       : constant Void_Ptr := Conv (MAP_FAILED_VALUE);
+
+   type Memory_Map_T is tagged limited record
+      My_Mapping : Void_Ptr := MAP_FAILED;
+      My_Length  : Size_T;
+   end record;
+
+   function Has_Mapping (Map : Memory_Map_T) return Boolean is (Map.My_Mapping /= MAP_FAILED);
+
+   function Mapping (Map : Memory_Map_T) return Void_Ptr is (Map.My_Mapping);
 
 end Px;
