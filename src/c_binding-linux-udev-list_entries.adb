1167221554c1980308e7fd349bab93a8166c6810
Joakim Strandberg <joakim@mequinox.se>
2018-11-14 00:26:12 +0100

    Added all C-functions to the public API

--- src/c_binding-linux-udev-list_entries.adb
+++ src/c_binding-linux-udev-list_entries.adb
@@ -6,11 +6,13 @@ package body C_Binding.Linux.Udev.List_Entries is
    --  On success, return a pointer to the requested list entry.
    --  If no such entry can be found, or on failure, null is returned.
 
---     function Udev_List_Entry_Get_By_Name
---       (Arg1 : System.Address;
---        Arg2 : Interfaces.C.Strings.Chars_Ptr) return System.Address;
---     pragma Import
---       (C, Udev_List_Entry_Get_By_Name, "udev_list_entry_get_by_name");
+   function Udev_List_Entry_Get_By_Name
+     (Current : Udev_List_Entry_Ptr;
+      Name    : C_String) return Udev_List_Entry_Ptr;
+   pragma Import
+     (C, Udev_List_Entry_Get_By_Name, "udev_list_entry_get_by_name");
+   --  Returns the entry where name matched,
+   --  null if no matching entry is found.
 
    function Udev_List_Entry_Get_Name
      (Arg1 : Udev_List_Entry_Ptr) return Interfaces.C.Strings.Chars_Ptr;
@@ -27,23 +29,34 @@ package body C_Binding.Linux.Udev.List_Entries is
    --  representing the requested value. The string is bound to
    --  the lifetime of the list entry itself. On failure, null is returned.
 
-   procedure Next (LE : in out List_Entry) is
+   procedure Next (List_Entry : in out List_Entries.List_Entry) is
    begin
-      LE.My_Ptr := Udev_List_Entry_Get_Next (LE.My_Ptr);
+      List_Entry.My_Ptr := Udev_List_Entry_Get_Next (List_Entry.My_Ptr);
    end Next;
 
-   function Name (LE : List_Entry) return String_Result is
+   function Name (List_Entry : List_Entries.List_Entry) return String_Result is
       Text : constant Interfaces.C.Strings.Chars_Ptr
-        := Udev_List_Entry_Get_Name (LE.My_Ptr);
+        := Udev_List_Entry_Get_Name (List_Entry.My_Ptr);
    begin
       return Get_String_Result (Text, "List entry name failure");
    end Name;
 
-   function Value (LE : List_Entry) return String_Result is
+   function Value (List_Entry : List_Entries.List_Entry) return String_Result is
       Text : constant Interfaces.C.Strings.Chars_Ptr
-        := Udev_List_Entry_Get_Value (LE.My_Ptr);
+        := Udev_List_Entry_Get_Value (List_Entry.My_Ptr);
    begin
       return Get_String_Result (Text, "List entry value failure");
    end Value;
+
+   procedure Get_By_Name
+     (
+      Current : List_Entry;
+      Name    : String;
+      Found_Entry : out List_Entry
+     ) is
+   begin
+      Found_Entry.My_Ptr
+        := Udev_List_Entry_Get_By_Name (Current.My_Ptr, +Name);
+   end Get_By_Name;
 
 end C_Binding.Linux.Udev.List_Entries;
--- src/c_binding-linux-udev-list_entries.ads
+++ src/c_binding-linux-udev-list_entries.ads
@@ -1,18 +1,32 @@
 package C_Binding.Linux.Udev.List_Entries is
 
-   type List_Entry is new List_Entry_Base with null record;
+   type List_Entry;
 
-   function Exists (LE : List_Entry) return Boolean;
+   procedure Get_By_Name
+     (
+      Current : List_Entry;         --  current entry
+      Name    : String;             --  name string to match
+      Found_Entry : out List_Entry  --  The entry where name matched
+     ) with
+       Pre => List_Entries.Exists (Current);
+   --  On success, Found_Entry.Exists = True
+   --  On failure, Found_Entry.Exists = False
 
-   procedure Next (LE : in out List_Entry) with
-     Pre => LE.Exists;
+   type List_Entry is new List_Entry_Base with private;
 
-   function Name  (LE : List_Entry) return String_Result;
-   function Value (LE : List_Entry) return String_Result;
+   function Exists (List_Entry : List_Entries.List_Entry) return Boolean;
+
+   procedure Next (List_Entry : in out List_Entries.List_Entry) with
+     Pre => List_Entry.Exists;
+
+   function Name  (List_Entry : List_Entries.List_Entry) return String_Result;
+   function Value (List_Entry : List_Entries.List_Entry) return String_Result;
 
 private
 
-   function Exists (LE : List_Entry) return Boolean is
-     (LE.My_Ptr /= null);
+   type List_Entry is new List_Entry_Base with null record;
+
+   function Exists (List_Entry : List_Entries.List_Entry) return Boolean is
+     (List_Entry.My_Ptr /= null);
 
 end C_Binding.Linux.Udev.List_Entries;
