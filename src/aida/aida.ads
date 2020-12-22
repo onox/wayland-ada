1b17e931c97477f5e881089141655257ae85e2ea
onox <denkpadje@gmail.com>
2020-12-22 01:33:45 +0100

    Clean up package Aida
    Signed-off-by: onox <denkpadje@gmail.com>

--- src/aida/aida.ads
+++ src/aida/aida.ads
@@ -14,70 +14,46 @@
 --  See the License for the specific language governing permissions and
 --  limitations under the License.
 
-with Ada.Containers;
+package Aida with SPARK_Mode is
+   pragma Preelaborate;
 
-package Aida with Pure, SPARK_Mode is
-
-   subtype Digit_Character is Character with
-     Predicate =>
-       Digit_Character = '0' or
-       Digit_Character = '1' or
-       Digit_Character = '2' or
-       Digit_Character = '3' or
-       Digit_Character = '4' or
-       Digit_Character = '5' or
-       Digit_Character = '6' or
-       Digit_Character = '7' or
-       Digit_Character = '8' or
-       Digit_Character = '9';
-   --  It is more useful with a subtype for digit characters than
-   --  an function Is_Digit (C : Character) return Boolean;
-   --  Consider for example case-statements:
-   --
-   --  C : Character;
-   --
-   --  case Digit_Character (C) is
-   --     when '0' => ..
-   --     ...
-   --     when '9' => ..
-   --  end case;
-
-   function To_String (This : Integer) return String with
-     Global => null,
-     Post   =>
+   function To_String (This : Integer) return String
+     with Global => null,
+          Post   =>
        (if This < 0 then
           To_String'Result'Length >= 2 and To_String'Result'Length <= 11
             else
               To_String'Result'Length >= 1 and To_String'Result'Length <= 10);
 
    procedure To_Integer
-     (Source     : in  String;
+     (Source     : String;
       Target     : out Integer;
-      Has_Failed : out Boolean) with
-     Global         => null;
+      Has_Failed : out Boolean)
+   with Global => null;
 
-   type Call_Result is tagged limited private with
-     Default_Initial_Condition => Call_Result.Has_Failed = False;
+   type Call_Result is tagged limited private
+     with Default_Initial_Condition => Call_Result.Has_Failed = False;
 
-   procedure Initialize (This   : in out Call_Result;
-                         Code_1 : Integer;
-                         Code_2 : Integer) with
-     Global     => null,
-     Pre'Class  => not Has_Failed (This),
-     Post'Class => This.Has_Failed = True;
+   procedure Initialize
+     (This   : in out Call_Result;
+      Code_1 : Integer;
+      Code_2 : Integer)
+   with Global     => null,
+        Pre'Class  => not Has_Failed (This),
+        Post'Class => This.Has_Failed = True;
 
-   function Has_Failed (This : Call_Result) return Boolean with
-     Global => null;
+   function Has_Failed (This : Call_Result) return Boolean
+     with Global => null;
 
-   function Message (This : Call_Result) return String with
-     Global => null;
+   function Message (This : Call_Result) return String
+     with Global => null;
 
 private
 
    type Call_Result is tagged limited
       record
-         My_Code_1 : Integer := 0;
-         My_Code_2 : Integer := 0;
+         Code_1     : Integer := 0;
+         Code_2     : Integer := 0;
          My_Has_Failed : Boolean := False;
       end record;
 
